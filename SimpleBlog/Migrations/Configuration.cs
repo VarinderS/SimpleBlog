65aca1b02994740ca17c1a55952a6003c1cf9587
Varinder Singh <varinder@kudosweb.com>
2016-12-24 20:00:33 +1300

    Seeding admin user

--- SimpleBlog/Migrations/Configuration.cs
+++ SimpleBlog/Migrations/Configuration.cs
@@ -1,3 +1,8 @@
+using System.Linq.Expressions;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SimpleBlog.Models;
+
 namespace SimpleBlog.Migrations
 {
     using System;
@@ -14,18 +19,52 @@ namespace SimpleBlog.Migrations
 
         protected override void Seed(SimpleBlog.Models.SimpleBlogDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+			var roleStore = new RoleStore<IdentityRole>(context: context);
+
+			var roleManager = new RoleManager<IdentityRole>(store: roleStore);
+
+			var userStore = new UserStore<ApplicationUser>(context: context);
+
+			var userManager = new UserManager<ApplicationUser>(store: userStore);
+
+
+			var publicRole = roleManager.FindByName(roleName: "Public");
+
+			if (publicRole == null)
+			{
+				publicRole = new IdentityRole { Name = "Public" };
+
+				roleManager.Create(role: publicRole);
+			}
+
+
+			var adminRole = roleManager.FindByName(roleName: "Admin");
+
+			if (adminRole == null)
+			{
+				adminRole = new IdentityRole { Name = "Admin" };
+
+				roleManager.Create(role: adminRole);
+			}
+
+
+			var adminUser = userManager.FindByName(userName: "Varinder");
+
+			if (adminUser == null)
+			{
+				adminUser = new ApplicationUser { UserName = "Varinder", Email = "varinder@email.com" };
+
+				userManager.Create(user: adminUser, password: "password");
+			}
+
+
+			var adminUserCreated = userManager.IsInRole(userId: adminUser.Id, role: adminRole.Name);
+
+			if (adminUserCreated == false)
+			{
+				userManager.AddToRole(userId: adminUser.Id, role: adminRole.Name);
+			}
         }
     }
 }
