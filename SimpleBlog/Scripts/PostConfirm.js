e8a57a264ad401e9aa1a2e27f3cb526290c20f23
Varinder Singh <varinder@kudosweb.com>
2016-12-25 21:02:22 +1300

    Moved javascript to a separate file

---
+++ SimpleBlog/Scripts/PostConfirm.js
@@ -0,0 +1,32 @@
+﻿(function ($) {
+
+	var $confirmItems = $("[data-confirm]");
+
+	var $body = $("body");
+
+	$confirmItems.on("click", function (e) {
+
+		e.preventDefault();
+
+		var $this = $(this);
+
+		var url = $this.attr("href");
+
+		var confirmation = $this.attr("data-confirm");
+
+		var confirmed = true;
+
+		if (confirmation) {
+
+			confirmed = window.confirm(confirmation);
+		}
+
+		if (confirmed) {
+
+			var $antiForgeryTokenElement = $(".js-anti-forgery-token").clone();
+
+			$("<form/>").attr("action", url).attr("method", "post").append($antiForgeryTokenElement).appendTo($body).submit();
+		}
+	});
+
+})(jQuery);
